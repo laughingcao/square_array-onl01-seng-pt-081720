4e130945a6e0467c1dfc308a8466b1fae2dd27e5
William T Cao <williamacao@gmail.com>
2020-08-27 18:09:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
-  numbers = [1,2,3]
-  
-new_numbers = [9, 10, 16, 25]
- square_array**
- 
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
 end
 
+my_arr = [1, 2]
+p square_array(my_arr) #=> [1, 4]
